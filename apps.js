1c04278e10df3b4a5d45bfa7ea907196684ab731
gandhi <gandhiflores@gmail.com>
2022-06-05 20:09:38 -0500

    Grafica Persona vs Salario

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 //apps.js
 //fetch data from the api
 
-async function apiData(){
+/* async function apiData(){
   const apiLink="http://dummy.restapiexample.com/api/v1/employees";
 
   const response = await fetch(apiLink)
@@ -15,4 +15,18 @@ async function apiData(){
   const fullname = datapoints.data.map((name)=> name.employee_name)
   console.log(fullname)
   employeeFullName = fullname
+}  */
+async function apiData(){
+  const apilink =  "http://dummy.restapiexample.com/api/v1/employees";
+  const response = await fetch(apilink);
+  const datapoint = await response.json();
+  console.log(datapoint);
+  const salary = datapoint.data.map((amout)=>amout.employee_salary);
+  console.log(salary);
+  employedSalariData = salary;
+
+  const fullname = datapoint.data.map((name)=>name.employee_name);
+  console.log(fullname);
+  employedFullName=fullname;
+
 }
